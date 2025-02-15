f821ed86de4d6d3e64abac4e1032c9203b0189ce
praveen-1101 <131389787+praveen-1101@users.noreply.github.com>
2025-02-15 12:29:17 +0530

    Update Rating.js

--- backend/models/Rating.js
+++ backend/models/Rating.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 
 const RatingSchema = new mongoose.Schema({
     subjectId: 
@@ -13,6 +13,32 @@ const RatingSchema = new mongoose.Schema({
         required:true, 
         min:1, 
         max:4
+    }
+});
+
+module.exports = mongoose.model('Rating', RatingSchema);
+ */
+
+
+
+const mongoose = require('mongoose');
+
+const RatingSchema = new mongoose.Schema({
+    subjectId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Subject', 
+        required: true 
+    },
+    rating: {
+        type: Number,
+        required: true, 
+        min: 1, 
+        max: 4
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
     }
 });
 
